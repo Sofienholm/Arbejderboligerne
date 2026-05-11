19a9078981a3af79d74e9b9e4dcade0a5b10bf5d
Sofienholm <eaa24snho@students.eaaa.dk>
2026-05-11 02:13:46 +0200

    s

--- src/screens/Guide/Guide.jsx
+++ src/screens/Guide/Guide.jsx
@@ -1,15 +1,60 @@
 import styles from "./Guide.module.css";
 
+import step1 from "../../assets/images/illustrations/Guide-step1.svg";
+import step2 from "../../assets/images/illustrations/Guide-step2.svg";
+import step3 from "../../assets/images/illustrations/Guide-step3.svg";
+import step4 from "../../assets/images/illustrations/Guide-step4.svg";
+import arrowRight from "../../assets/images/illustrations/PilThojre.svg";
+import arrowLeft from "../../assets/images/illustrations/PilTvenstre.svg";
+
 export default function Guide({ onNext }) {
   return (
     <section className={styles.screen}>
-      <h1 className={styles.title}>SÅDAN HJÆLPER DU</h1>
+      <div className={styles.header}>
+        <h1 className={styles.title}>DETALJ I FORTÆLLINGEN</h1>
 
-      <div className={styles.steps}>
-        <p>1. Scan QR-koden.</p>
-        <p>2. Gå ind i lejligheden.</p>
-        <p>3. Find spor i rummet.</p>
-        <p>4. Hjælp familien med dilemmaet.</p>
+        <p className={styles.subtitle}>
+          OG HJÆLP FAMILIEN MED DERES DILEMMAER
+        </p>
+      </div>
+
+
+      <img className={styles.stepImage1} src={step1} alt="" />
+      <img className={styles.arrowRight1} src={arrowLeft} alt="" />
+
+      <img className={styles.stepImage2} src={step2} alt="" />
+      <img className={styles.arrowLeft} src={arrowRight} alt="" />
+
+      <img className={styles.stepImage3} src={step3} alt="" />
+      <img className={styles.arrowRight2} src={arrowLeft} alt="" />
+
+      <img className={styles.stepImage4} src={step4} alt="" />
+
+      <div className={styles.stepText1}>
+        <h2>Scan først QR koden</h2>
+        <p>SCAN KODEN FOR AT STARTE OPLEVELSEN.</p>
+      </div>
+
+      <div className={styles.stepText2}>
+        <h2>Gå ind i lejligheden</h2>
+        <p>TRÆD IND I HJEMMET OG OPLEV HVERDAGEN TÆT PÅ.</p>
+      </div>
+
+      <div className={styles.stepText3}>
+        <h2>Find spor i rummet</h2>
+        <p>FIND HOTSPOTS OG UNDERSØG GENSTANDE MED DIN TELEFON.</p>
+      </div>
+
+      <div className={styles.stepText4}>
+        <h2>Løs dilemmaet</h2>
+        <p>SAML SPORENE OG FIND SVARET.</p>
+      </div>
+
+      <div className={styles.timeline}>
+        <span />
+        <span />
+        <span />
+        <span />
       </div>
 
       <button className={styles.button} onClick={onNext}>
