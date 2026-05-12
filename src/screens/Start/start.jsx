c65d34c9d69faa9c2ee13e356a2a9d9a1dad4758
Martinlpilgaard <eaa24mlpk@students.eaaa.dk>
2026-05-12 10:44:46 +0200

    done

--- src/screens/Start/start.jsx
+++ src/screens/Start/start.jsx
@@ -9,23 +9,27 @@ import star1 from "../../assets/images/illustrations/star1.svg";
 import star2 from "../../assets/images/illustrations/star2.svg";
 import star3 from "../../assets/images/illustrations/star3.svg";
 import star4 from "../../assets/images/illustrations/star4.svg";
+import star5 from "../../assets/images/illustrations/star5.svg";
 import star6 from "../../assets/images/illustrations/star6.svg";
 import star11 from "../../assets/images/illustrations/star11.svg";
+import star12 from "../../assets/images/illustrations/star12.svg";
 
 export default function Start({ onNext }) {
   return (
     <div className={styles.container} onClick={onNext}>
       {/* svævende baggrundselementer */}
       <div className={styles.bgLayer}>
-        <img src={radio} alt="" className={`${styles.bg} ${styles.radio} floatA`} />
+        <img src={radio} alt="" className={`${styles.bg} ${styles.radio} floatC`} />
         <img src={tv} alt="" className={`${styles.bg} ${styles.tv} floatB`} />
 
         <img src={star1} alt="" className={`${styles.bg} ${styles.star1} floatC`} />
         <img src={star2} alt="" className={`${styles.bg} ${styles.star2} floatA`} />
         <img src={star3} alt="" className={`${styles.bg} ${styles.star3} floatB`} />
         <img src={star4} alt="" className={`${styles.bg} ${styles.star4} floatC`} />
+        <img src={star5} alt="" className={`${styles.bg} ${styles.star5} floatA`} />
         <img src={star6} alt="" className={`${styles.bg} ${styles.star6} floatA`} />
         <img src={star11} alt="" className={`${styles.bg} ${styles.star11} floatB`} />
+        <img src={star12} alt="" className={`${styles.bg} ${styles.star12} floatC`} />
       </div>
 
       {/* overskrift */}
