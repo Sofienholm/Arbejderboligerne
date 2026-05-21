59fcea3a52a7a0ba42f0943404f4e22bad0e5ae6
Sofienholm <eaa24snho@students.eaaa.dk>
2026-05-21 14:54:04 +0200

    m

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import TabletFrame from "./TabletFrame";
 import "./styles/global.css";
 
+// Kører vi inde i tablet-rammen (iframe) eller som den ydre "shell"?
+const params = new URLSearchParams(window.location.search);
+const isEmbedded = params.has("embed") || window.self !== window.top;
+
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <StrictMode>{isEmbedded ? <App /> : <TabletFrame />}</StrictMode>
 );
